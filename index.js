c8a444574eeed289df2a979e3f2f811d00df61a0
gary <gary.white@lodgit.com>
2022-01-27 13:09:03 +0100

    code working

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-function checkAlive (health) {
-    if () {
-      health < 0
-      return false
-    } else () {
-      return true
+function checkAlive(health) {
+    if (health > 0) {
+      return true;
+    } else {
+      return false;
     }
 }
 
 console.log(checkAlive(5)); // true
-console.log(checkAlive(10)); // false
+console.log(checkAlive(0)); // false
