549bf62ca60fef63b8904f7161c1b2171c9ba77f
gary <gary.white@lodgit.com>
2022-01-27 13:04:26 +0100

    template

---
+++ index.js
@@ -0,0 +1,11 @@
+function checkAlive (health) {
+    if () {
+      health < 0
+      return false
+    } else () {
+      return true
+    }
+}
+
+console.log(checkAlive(5)); // true
+console.log(checkAlive(10)); // false
